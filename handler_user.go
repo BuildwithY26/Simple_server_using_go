c294b94ce2e14937a560841edb356e79ba306ab4
Yashash <yashumallesh2022@gmail.com>
2026-08-27 17:10:38 +0530

    Update handler_user.go

--- handler_user.go
+++ handler_user.go
@@ -68,14 +68,14 @@ func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request, 
 }
 
 func (apiCfg *apiConfig) handlerGetPostsForUser(w http.ResponseWriter, r *http.Request, user database.User) {
-		posts, err := apiCfg.DB.GetPostsForUser(r.Context(), GetPostsForUserParams(
+		posts, err := apiCfg.DB.GetPostsForUser(r.Context(), database.GetPostsForUserParams{
 			user.ID,
 			10,
-		))
+		})
 		if err != nil {
 		respondwithError(w, http.StatusInternalServerError, "Failed to get post")
-		return
+		return 
 	}
 
-	return
+	respondwithJSON(w, http.StatusOK, posts)
 }
